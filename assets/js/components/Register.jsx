4938dcba9311967b37bfc860e2ce7cbc802f8f35
Saumya Garg <SaumyaGarg@gmail.com>
2017-07-19 07:53:57 -0400

    updated manage.py

--- assets/js/components/Register.jsx
+++ assets/js/components/Register.jsx
@@ -7,7 +7,38 @@ class Register extends React.Component {
 
     console.log('i am in the render for register');
   return (
-  	<Container></Container>
+  	<Container>
+     
+    <Row><Col  xs="6">
+      <h1>Login</h1>
+      <Form>
+        <FormGroup>
+          <Label for="exampleEmail">Email</Label>
+          <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+        </FormGroup>
+        <FormGroup>
+            <Label for="last_name">Last Name</Label>
+            <Input type="text"  id="last_name" name="last_name" className="form-control" placeholder="please enter last name" required="" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="examplePassword">Password</Label>
+          <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
+     </Col></Row>
+     <Row>
+      <Col  xs="6">
+       <Link to='register'>Register</Link>
+      </Col>
+     </Row>
+          <Row>
+      <Col  xs="6">
+      <a>Forgot Password</a>
+      </Col>
+     </Row>
+
+    </Container>
   );
 }
 
