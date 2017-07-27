a50caa089d9f4a339e8d13fc273604cd19320a92
Saumya Garg <SaumyaGarg@gmail.com>
2017-07-27 02:26:05 -0400

    get oauth token

--- jvbhealthwellness/urls.py
+++ jvbhealthwellness/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
-
+from users import views as userViews
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^djangojs/', include('djangojs.urls')),
+    url(r'^djangojs/', include('djangojs.urls')),
+    url(r'^users/receive_token$', userViews.request_token, name='receive_token'),
+    url(r'^users/request_token$',userViews.request_token,name='request_token'),
 
     url(r'^$', TemplateView.as_view(template_name='exampleapp/reg.html'), name='home'),
     url(r'^register$', TemplateView.as_view(template_name='exampleapp/reg.html'), name='home'),
--- users/views.py
+++ users/views.py
@@ -1,4 +1,71 @@
 from django.shortcuts import render  # noqa
-
+from rauth import OAuth1Service
+import webbrowser 
+from django.shortcuts import redirect
 
 # Create your views here.
+
+
+# In state=1 the next request should include an oauth_token.
+#If it doesn't go back to 0
+
+
+def request_token(request):
+    req_url = 'http://connectapitest.garmin.com/oauth-service-1.0/oauth/request_token'
+    authurl = 'http://connecttest.garmin.com/oauthConfirm'
+    acc_url = 'http://connectapitest.garmin.com/oauth-service-1.0/oauth/access_token'
+    conskey = 'YOUR_KEY';
+    conssec = 'YOUR_SECRET';
+    session = request.session
+    if not 'auth_token' in session and ('state' in session and session['state'])==1:
+        session['state'] = 0; 
+    # try:
+
+    service = OAuth1Service(
+          # name = 'etrade',
+          consumer_key = conskey,
+          consumer_secret = conssec,
+          request_token_url = req_url,
+          access_token_url = acc_url,
+          authorize_url = authurl, 
+          # base_url = 'https://etws.etrade.com'
+          )
+
+    # Get request token and secret    
+    # $oauth = new OAuth($conskey,$conssec,OAUTH_SIG_METHOD_HMACSHA1,OAUTH_AUTH_TYPE_URI)
+    # $oauth->enableDebug()
+
+    session = request.session
+    
+    if not 'oauth_token' in session and not ('state' in session or session['state']):
+        # request_token, request_token_secret = service.get_request_token(params = 
+  #                     {'oauth_callback': 'oob', 
+  #                      'format': 'json'})
+
+        request_token, request_token_secret = service.get_request_token()               
+        # $request_token_info = $oauth->getRequestToken($req_url)
+        session['request_token'] = request_token
+        session['request_token_secret'] = request_token_secret
+        session['state'] = 1
+        redirect(authurl + '?oauth_token={0}'.format(request_token))
+    elif 'state' in session and session['state'] ==1:
+        #oauth token already exists
+        #determine access token temporaily for work  
+        #continue
+        access_token, access_token_secret = service.get_access_token(session['request_token'],
+            session['request_token_secret'])
+
+        # need to validate that the token still works.... not done
+        session['state'] = 2
+        session['access_token'] = access_token
+        session['access_secret'] = access_token_secret
+    # except Exception, e:
+    #     print(e)
+    
+def receive_token(request):
+    print(request)
+    # $json = json_decode($_POST['uploadMetaData']);
+    # $tmp_name = $_FILES['file']['tmp_name'];
+    # $file_name = $_FILES['file']['name']; 
+    # move_uploaded_file($tmp_name, YOUR_FILE_PATH); 
+    # header('Location: YOUR_URL_FOR_THE_SAVED_FILE', true, 201);
