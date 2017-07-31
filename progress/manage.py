0934351ca07c70cc8819c7cb84501a5cd6effacf
Saumya Garg <SaumyaGarg@gmail.com>
2017-07-31 14:44:03 -0400

    remove bad merge

--- progress/manage.py
+++ progress/manage.py
@@ -4,26 +4,6 @@
 import os
 import sys
 
-from decouple import config
-
-
-if __name__ == "__main__":
-    settings_module = config('DJANGO_SETTINGS_MODULE', default=None)
-
-    if sys.argv[1] == 'test':
-        if settings_module:
-            print("Ignoring config('DJANGO_SETTINGS_MODULE') because it's test. "
-                  "Using 'jvbhealthwellness.settings.test'")
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jvbhealthwellness.settings.test")
-    else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
-
-    from django.core.management import execute_from_command_line
-
-=======
-import os
-import sys
-
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Program_Annalyzer.settings")
     try:
