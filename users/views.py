c10eabe3b76c937f825db1a5f1e1d0f0c2e29d29
Saumya Garg <SaumyaGarg@gmail.com>
2017-08-06 13:33:05 -0400

    request_token_session to request_token_secret

--- users/views.py
+++ users/views.py
@@ -115,7 +115,7 @@ def receive_token(request):
     #oauth_token=d37f1145-59b1-4f85-bc18-9a25e5697445&oauth_verifier=d9lZlU521B
 
 
-    access_token, access_token_secret = service.get_access_token(oauth_token, session['request_token_session'])
+    access_token, access_token_secret = service.get_access_token(oauth_token, session['request_token_secret'])
 
     # need to validate that the token still works.... not done
     session['state'] = 2
