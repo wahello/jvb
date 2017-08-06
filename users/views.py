2ba3b6ddf7091be3580f5d29e1a6119344435717
Saumya Garg <SaumyaGarg@gmail.com>
2017-08-06 15:47:34 -0400

    added logging to make things more clear

--- users/views.py
+++ users/views.py
@@ -13,6 +13,20 @@ from rest_framework import authentication, permissions
 
 from django.views.decorators.clickjacking import xframe_options_exempt
 import urllib
+import logging
+
+try:
+    import http.client as http_client
+except ImportError:
+    # Python 2
+    import httplib as http_client
+http_client.HTTPConnection.debuglevel = 1
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
 
 
 
