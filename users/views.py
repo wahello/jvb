a970785a44f40019e1767b544aec1188988943f6
Saumya Garg <SaumyaGarg@gmail.com>
2017-08-08 00:36:53 -0400

    adjusting user views to json

--- users/views.py
+++ users/views.py
@@ -148,7 +148,7 @@ def receive_token(request):
     # print(access_token_secret)
     session = service.get_auth_session(session['request_token'],session['request_token_secret'],method='POST',data=data)
     r = session.get('https://healthapi.garmin.com/wellness-api/rest/dailies?uploadStartTimeInSeconds=1452470400&uploadEndTimeInSeconds=1502150488', params={'format': 'json'})
-    print(r.text)
+    print(r.json())
 
     return redirect('/service_connect')
     
