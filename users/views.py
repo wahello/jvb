d13cc5d5e4ca29d02ad7c750c23be2d416be9801
Saumya Garg <SaumyaGarg@gmail.com>
2017-08-08 00:07:33 -0400

    took out the access token piece for now

--- users/views.py
+++ users/views.py
@@ -136,16 +136,16 @@ def receive_token(request):
         'oauth_verifier': oauth_verifier,
     }
 
-    access_token, access_token_secret = service.get_access_token(session['request_token'], session['request_token_secret'],method='POST',data=data)
+    # access_token, access_token_secret = service.get_access_token(session['request_token'], session['request_token_secret'],method='POST',data=data)
 
-    # need to validate that the token still works.... not done
-    session['state'] = 2
-    session['access_token'] = access_token
-    session['access_secret'] = access_token_secret
-    print('access token')
-    print(access_token)
-    print('access_token_secret')
-    print(access_token_secret)
+    # # need to validate that the token still works.... not done
+    # session['state'] = 2
+    # session['access_token'] = access_token
+    # session['access_secret'] = access_token_secret
+    # print('access token')
+    # print(access_token)
+    # print('access_token_secret')
+    # print(access_token_secret)
     session = service.get_auth_session(session['request_token'],session['request_token_secret'],method='POST',data=data)
     r = session.get('https://healthapi.garmin.com/wellness-api/rest/dailies?uploadStartTimeInSeconds=1452470400&uploadEndTimeInSeconds=1502150488', params={'format': 'json'})
     print(r.text)
