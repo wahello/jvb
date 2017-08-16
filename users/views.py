ee8acd560443a9cd47b9586a3c6f1ed21f447a7a
Saumya Garg <SaumyaGarg@gmail.com>
2017-08-16 21:01:30 +0530

    adding params from headers into the post dictionary

--- users/views.py
+++ users/views.py
@@ -122,10 +122,8 @@ def receive_token(request):
     encoded_verifier = urllib.parse.quote(oauth_verifier)
     # xacc_url = '{0}?oauth_verifier={1}'.format(acc_url,encoded_verifier)    
     
-    data = {'oauth_verifier': oauth_verifier,
-        'oauth_token': oauth_token,
-        'oauth_token_secret': session['request_token_secret']
-    }
+
+
 
     from requests_oauthlib import OAuth1, OAuth1Session
     s = requests.Session()
@@ -134,6 +132,16 @@ def receive_token(request):
     s.headers.update({'oauth_verifier': oauth_verifier,
         'oauth_token': oauth_token,
         'oauth_token_secret': session['request_token_secret'] })
+
+    data = {'oauth_verifier': oauth_verifier,
+        'oauth_token': oauth_token,
+        'oauth_token_secret': session['request_token_secret'], 
+        'oauth_timestamp': s.headers['oauth_timestamp'],
+        'oauth_nonce': s.headers['oauth_nonce'],
+        'oauth_signature_method': s.headers['oauth_signature_method']
+    }
+
+
     r = s.get(acc_url, params=data)
     print(r.text)
     print(r.json())
@@ -151,8 +159,6 @@ def receive_token(request):
     # #oauth_token=d37f1145-59b1-4f85-bc18-9a25e5697445&oauth_verifier=d9lZlU521B
 
     # print('oauth_token_secret')
-
-
     # access_token, access_token_secret = service.get_access_token(session['request_token'], session['request_token_secret'],method='GET', header_auth=True)
 
     # # need to validate that the token still works.... not done
