9e787f8612c1834df192755cfec6f46402ab1a4c
Swapnika <swapnika.gaddam@gmail.com>
2017-08-31 15:55:21 +0530

    garmindatapull

---
+++ assets/js/src/components/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import GarminDataPage from './garmin_data';
+
+
+export default class App extends Component {
+
+
+  render() {
+
+    return (
+      <div>
+        <GarminDataPage />
+      </div>
+    );
+  }
+}
