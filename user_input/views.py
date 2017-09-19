16862195730d4359ff80399feedbbab344163d1b
atul <atulk@s7inc.co>
2017-09-19 18:54:09 +0530

    fix 403 userinput

--- user_input/views.py
+++ user_input/views.py
@@ -1,3 +1,6 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
@@ -5,6 +8,7 @@ from .serializers import UserDailyInputSerializer
 
 from .models import UserDailyInput
 
+@method_decorator(csrf_exempt, name='dispatch')
 class UserDailyInputView(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated,)
     queryset = UserDailyInput.objects.all()
