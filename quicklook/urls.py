f88813f91ac33fa6b97cae2b07697e02f5a84734
atul <atulk@s7inc.co>
2017-09-20 17:37:27 +0530

    fix minor bugs in urls regex

--- quicklook/urls.py
+++ quicklook/urls.py
@@ -5,26 +5,26 @@ urlpatterns = [
 	url(r'^users/data$',views.UserQuickLookView.as_view(),
 								  name="quick_look"),
 
-	url(r'^users/data/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})$',
+	url(r'^users/data/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})$',
 						views.UserQuickLookItemView.as_view(),name="quicklook_item"),
 
 	url(r'^users/grades$',views.GradeListView.as_view(),
 								name="grades_list"),
-	url(r'^users/grades/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})$',
+	url(r'^users/grades/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})$',
 						views.GradeItemView.as_view(),name="grade_item"),
 
-	url(r'^users/grades/weekly/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})$',
+	url(r'^users/grades/weekly/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})$',
 						views.GradeWeeklyListView.as_view(),name="grade_weekly_list"),
 
 	url(r'^users/steps$',views.StepListView.as_view(),
 								name="steps_list"),
 
-	url(r'^users/steps/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})$',
+	url(r'^users/steps/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})$',
 						views.StepsItemView.as_view(),name="step_item"),
 
 	url(r'^users/sleep$',views.SleepListView.as_view(),
 								name="sleep_list"),
 
-	url(r'^users/sleep/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})$',
+	url(r'^users/sleep/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})$',
 						views.SleepItemView.as_view(),name="sleep_item"),
 ]
