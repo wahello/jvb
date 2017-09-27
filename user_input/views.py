741ef0d36960e66b2ea604fc7a1b29874a5b1c0a
atul <atulk@s7inc.co>
2017-09-28 00:29:48 +0530

    Fixed login issue, removed TokenAuthentication class and added custom CsrfExemptSessionAuthentication

--- user_input/views.py
+++ user_input/views.py
@@ -1,11 +1,17 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication 
 
 from .serializers import UserDailyInputSerializer
 
 from .models import UserDailyInput
 
+class CsrfExemptSessionAuthentication(SessionAuthentication):
+	def enforce_csrf(self, request):
+		return
+
 class UserDailyInputView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
-    queryset = UserDailyInput.objects.all()
-    serializer_class = UserDailyInputSerializer
+	authentication_classes = (CsrfExemptSessionAuthentication,)
+	permission_classes = (IsAuthenticated,)
+	queryset = UserDailyInput.objects.all()
+	serializer_class = UserDailyInputSerializer
