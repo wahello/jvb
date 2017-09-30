d2ac3ef63c2a61a43cfd5a38ae0187f22485843c
Saumya Garg <SaumyaGarg@gmail.com>
2017-09-30 06:46:13 -0700

    home page react update

--- assets/js/components/HomePageReactTitle.jsx
+++ assets/js/components/HomePageReactTitle.jsx
@@ -104,7 +104,18 @@ class HomePageReactTitle extends Component {
                         </div>
                         </div>
                     </div>
+                    <div className="row">
+                        <div className="col-sm-6 col-sm-offset-3 social-login">
+                          <h3>Link's</h3>
+                          <div className="social-login-buttons">
+                          <Link to='/users/dashboard'>Dashboard</Link><br/>
 
+                          <Link to='register'>Register</Link><br/>
+                            <Link to="forgotpassword">Forgot Password</Link><br/>
+
+                          </div>
+                        </div>
+                    </div>
 
               </div>
 
