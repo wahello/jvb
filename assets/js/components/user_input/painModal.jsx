55885b42c063c59f8ac1494692b4cbdd23bb14a9
atul <atulk@s7inc.co>
2017-10-13 15:51:18 +0530

    user input form minor bug fix

--- assets/js/components/user_input/painModal.jsx
+++ assets/js/components/user_input/painModal.jsx
@@ -38,15 +38,17 @@ export default class PainModal extends Component{
 		const area = this.props.pain_area;
 		const area_list = area.split(',');
 
-		for(let item of option){
-			for(let a of area_list){
-				if(item.value === a){
-					other = false;
-					break;
+		if(area !== ""){
+			for(let item of option){
+				for(let a of area_list){
+					if(item.value === a){
+						other = false;
+						break;
+					}
 				}
 			}
 		}
-
+		
 		this.state = {
 			collapse:other ? true : false,	
 			disabled: false,
