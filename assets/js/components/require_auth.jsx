deb42bd8b738febe14423354d4c8ad617ea8e419
Suresh K <suresh@s7inc.co>
2017-11-01 21:55:05 +0530

    updating access and some userinput changes

--- assets/js/components/require_auth.jsx
+++ assets/js/components/require_auth.jsx
@@ -1,40 +1,34 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
-
 import {loadLocalState} from './localStorage';
-
 
 // A Wrapper component which will redirect the
 // user to homepage if user is not auhenticated
-
 export default function(ComposedComponent) {
- class Authentication extends Component{
-		static contextTypes ={
-			router: PropTypes.object
-		};
-
-		componentWillMount(){
-			const persisted_state = loadLocalState();
-			if((persisted_state && !persisted_state.authenticated) || 
-				persisted_state === undefined){
-				this.context.router.history.push('/');
-			}
-		}
-
-		componentWillUpdate(nextProps){
-			const persisted_state = loadLocalState();
-			if((persisted_state && !persisted_state.authenticated) ||
-				persisted_state === undefined){
-				this.context.router.history.push('/');
-			}
-		}
-
-		render(){
-			return(
-				<ComposedComponent {...this.props} />
-			);
-		}
-	}
-	return Authentication;
+    class Authentication extends Component{
+        static contextTypes = {
+            router: PropTypes.object
+        };
+        componentWillMount(){
+            const persisted_state = loadLocalState();
+            if((persisted_state && !persisted_state.authenticated) || 
+                persisted_state === undefined){
+                this.context.router.history.push('/');
+            }
+        }
+        componentWillUpdate(nextProps){
+            const persisted_state = loadLocalState();
+            if((persisted_state && !persisted_state.authenticated) ||
+                persisted_state === undefined){
+                this.context.router.history.push('/');
+            }
+        }
+        render(){
+            return(
+                <ComposedComponent {...this.props} />
+            );
+        }
+    }
+    return Authentication;
 }
