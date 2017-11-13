229d27a2839afaf1e9ec1101d9e0d68992a92961
Atul Kumar <atulk@s7inc.co>
2017-11-13 16:28:18 +0530

    Modified admin page for quick look and user input models

--- quicklook/admin.py
+++ quicklook/admin.py
@@ -10,32 +10,36 @@ from .models import ExerciseAndReporting,\
 					Grades,\
 					UserQuickLook
 
-class GradesInline(admin.TabularInline):
+class GradesInline(admin.StackedInline):
 	model = Grades
 
-class ExerciseAndReportingInline(admin.TabularInline):
+class ExerciseAndReportingInline(admin.StackedInline):
 	model = ExerciseAndReporting
 
-class SwimStatsInline(admin.TabularInline):
+class SwimStatsInline(admin.StackedInline):
 	model = SwimStats
 
-class BikeStatsInline(admin.TabularInline):
+class BikeStatsInline(admin.StackedInline):
 	model = BikeStats
 
-class StepsInline(admin.TabularInline):
+class StepsInline(admin.StackedInline):
 	model = Steps
 
-class SleepInline(admin.TabularInline):
+class SleepInline(admin.StackedInline):
 	model = Sleep
 
-class FoodInline(admin.TabularInline):
+class FoodInline(admin.StackedInline):
 	model = Food 
 
-class AlcoholInline(admin.TabularInline):
+class AlcoholInline(admin.StackedInline):
 	model = Alcohol
 
 class UserQuickLookAdmin(admin.ModelAdmin):
 	list_display=('user','created_at', 'updated_at')
+	list_filter = ('created_at','updated_at',)
+	save_on_top = True
+	search_fields = ('user__username','user__email','user__first_name',
+					 'user__last_name',)
 	inlines = [
 		GradesInline,
 		ExerciseAndReportingInline,
