f113041b3b6cfdbff7c724e3d7ba388c68289f8e
Atul Kumar <atulk@s7inc.co>
2017-11-16 16:17:43 +0530

    notifying admins on new user input

--- user_input/custom_signals.py
+++ user_input/custom_signals.py
@@ -1,3 +1,4 @@
 from django.dispatch import Signal
 
 user_input_post_save = Signal(providing_args=["request", "dt"])
+user_input_notify = Signal(providing_args=["request","instance","created"])
--- user_input/signals.py
+++ user_input/signals.py
@@ -1,10 +1,58 @@
-from .custom_signals import user_input_post_save
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
+
 from .serializers import UserDailyInputSerializer
 from quicklook.calculation_helper import create_quick_look
+from .custom_signals import user_input_post_save,user_input_notify
 
 @receiver(user_input_post_save, sender=UserDailyInputSerializer)
 def create_or_update_quicklook(sender, **kwargs):
 	request = kwargs.get('request')
 	dt = kwargs.get('dt').strftime("%Y-%m-%d")
 	# create_quick_look(request.user, dt)
+
+
+@receiver(user_input_notify, sender=UserDailyInputSerializer)
+def notify_admins(sender,instance=None, created=False,**kwargs):
+	'''
+		Send email to all the admin users with the admin link to 
+		newly created instance
+	'''
+	if created:
+		request = kwargs.get('request')
+		admin_users_email = [u.email for u in User.objects.filter(is_staff=True)]
+		info = (instance._meta.app_label, instance._meta.model_name)
+		instance_admin_url = request.build_absolute_uri('/').strip("/")+\
+							 reverse('admin:%s_%s_change' % info, args=(instance.pk,))
+		message = """
+Hi there,
+
+User {} ({}) has submitted new user inputs for {}. 
+Please click following link to access it - 
+{}
+
+If clicking the link above doesn't work, please copy and paste the URL in a new browser
+window instead.
+
+Sincerely,
+
+JVB Health & Wellness   
+ """
+		message = message.format(
+			instance.user.first_name+" "+instance.user.last_name,
+			instance.user.username,
+			instance.created_at.strftime("%b %d, %Y"),
+			instance_admin_url
+		)
+
+		if admin_users_email:
+			send_mail(
+				subject="New User Input",
+				message = message,
+				from_email = "saumyag@s7inc.co",
+				recipient_list = admin_users_email,
+				fail_silently = True  
+			)
