cb83032c6c2f8db0c8c0d3453e1c4e0fea47ae28
Atul Kumar <atulk@s7inc.co>
2018-01-10 17:44:18 +0530

    Bug fixes

--- assets/js/components/quicksummary/quicksummary_steps.jsx
+++ assets/js/components/quicksummary/quicksummary_steps.jsx
@@ -36,6 +36,8 @@ renderTableColumns(dateWiseData,category,classes=""){
 	                    if( mc != undefined && mc != "" && mc != "-"){
 	                        mc = JSON.parse(mc);
 	                        all_data.push(mc.inactive_hours);
+	                	}else{
+	                		all_data.push('-')
 	                	}
 	            	}
 	                else{
