ecffbc05952afbfb79f8484c9cae271e4e03253d
Suresh K <suresh@s7inc.co>
2018-03-19 19:55:41 +0530

    updating grades key link in home page

--- assets/js/components/dashboard/Dashboard.jsx
+++ assets/js/components/dashboard/Dashboard.jsx
@@ -31,8 +31,7 @@ class Dashboard extends Component {
 							   <Link to='/rawdata'>Raw Data</Link><br/>
 							   <Link to='/rawdata#movementconsistency'>Movement Consistency</Link><br/>
 							   <Link to='/rawdata#grades'>Grades</Link><br/>
-							   <a target="_blank" href = "https://s3.amazonaws.com/prod.tracker2/resource/87874259/Grades%20Key%202-7-18.pdf?response-content-disposition=inline&response-content-type=application%2Fpdf&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJJBSFJ4TCVKKGAIA%2F20180319%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20180319T065352Z&X-Amz-Expires=1800&X-Amz-SignedHeaders=host&X-Amz-Signature=b31f5e2328c5f7e2c9604e708b85f968f87615f73ff8e8afa4864c701e106e9f">Grades Key</a><br/>
-								{/*<Link to='/gradeskey'>Grades Key 1</Link><br/>*/}
+							   <a target="_blank" href = "/static/quicklook/grades_key.pdf">Grades Key</a><br/>
 							  {/*<Link to='/movement_consistency'>movement Consistency</Link><br/>*/}
 							  <h3 id="link_style">Set up Links to Garmin</h3>
 							  <a href='/users/request_token'>Garmin Health Connect</a><br/>
