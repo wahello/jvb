d03860e95cc1e3b929dc387ad57a1aee0af79c35
Atul Kumar <atulk@s7inc.co>
2018-04-05 18:27:13 +0530

    fixed small bug

--- leaderboard/helpers/leaderboard_helper_classes.py
+++ leaderboard/helpers/leaderboard_helper_classes.py
@@ -256,7 +256,7 @@ class LeaderboardOverview(object):
 	def _get_catg_score_priority(self):
 		categories = [x[0] for x in s.CATEGORY_CHOICES]
 		catg_score_priority = {}
-		lowest_first_categories = ['mc','alcohol_drink','resting_hr','awake_time']
+		lowest_first_categories = ['mc','resting_hr','awake_time']
 		for category in categories:
 			if category in lowest_first_categories:
 				catg_score_priority[category] = 'lowest_first'
