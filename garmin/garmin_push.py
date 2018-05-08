d5df7832c8fbec816881606ae00846a7a4a0ee3f
Atul Kumar <atulk@s7inc.co>
2018-05-08 18:49:43 +0530

    Added functions to store ping notification and updating state at various stage in pulling/storing health data

--- garmin/garmin_push.py
+++ garmin/garmin_push.py
@@ -14,6 +14,7 @@ from .models import (
     UserGarminDataStressDetails,
     UserGarminDataMetrics,
     UserGarminDataMoveIQ,
+    GarminPingNotification,
     # GarminConnectToken
 )
 
@@ -37,11 +38,40 @@ def _get_model_types():
 	}
 	return MODEL_TYPES
 
+def get_ping_summary_types():
+	return [
+		"dailies", "activities", "manuallyUpdatedActivities",
+		"epochs", "sleeps", "bodyComps", "stressDetails",
+		"moveIQActivities", "userMetrics", "deregistration"
+	]
+
 def _safe_get(data,attr,default):
 		data_item = data.get(attr,None)
 		if not data_item:
 			return default
 		return data_item
+
+def store_ping_notifications(obj,dtype,user):
+	callback_url = obj.get('callbackURL')
+	upload_start_time = None
+	if dtype != "deregistration":
+		upload_start_time = int(
+			re.search('uploadStartTimeInSeconds=(\d+)*',callback_url).group(1)
+		)
+	obj = GarminPingNotification.object.create(
+		user = user,
+		upload_start_time_seconds = upload_start_time,
+		notification = obj
+	)
+	return obj
+
+def update_notification_state(instance,state):
+	valid_states = [x[0] for x in GarminPingNotification.PING_STATE_CHOICES]
+	if state in valid_states:
+		instance.state = state
+	else
+		instance.state = "unprocessed"
+	instance.save()
 
 def _createObjectList(user,json_data,dtype,record_dt):
 	'''
@@ -121,73 +151,84 @@ def store_garmin_health_push(data):
 	conskey = '6c1a770b-60b9-4d7e-83a2-3726080f5556';
 	conssec = '9Mic4bUkfqFRKNYfM3Sy6i0Ovc9Pu2G4ws9';
 	print(data)
-	dtype = list(data.keys())[0]
+
 	MODEL_TYPES = _get_model_types()
-	for obj in data.get(dtype):
+	for dtype in data.keys():
+		if dtype in get_ping_summary_types():
+			for obj in data.get(dtype):
+				user_key = obj.get('userAccessToken')
+				try:
+					user = User.objects.get(garmin_token__token = user_key)
+				except User.DoesNotExist:
+					user = None
+					
+				if user:
+					# Store ping notification in database and update state to processing
+					ping_notif_obj = store_ping_notifications(obj,dtype,user)
+					update_notification_state(ping_notif_obj,"processing")
 
-		user_key = obj.get('userAccessToken')
-		try:
-			user = User.objects.get(garmin_token__token = user_key)
-		except User.DoesNotExist:
-			user = None
-			
-		if user:
-			callback_url = obj.get('callbackURL')
+					callback_url = obj.get('callbackURL')
 
-			access_token = user.garmin_token.token
-			access_token_secret = user.garmin_token.token_secret
+					access_token = user.garmin_token.token
+					access_token_secret = user.garmin_token.token_secret
 
-			service = OAuth1Service(
-				consumer_key = conskey,
-				consumer_secret = conssec,
-				request_token_url = req_url,
-				access_token_url = acc_url,
-				authorize_url = authurl, 
-			)
-			
-			upload_start_time = int(re.search('uploadStartTimeInSeconds=(\d+)*',
-								callback_url).group(1))
+					service = OAuth1Service(
+						consumer_key = conskey,
+						consumer_secret = conssec,
+						request_token_url = req_url,
+						access_token_url = acc_url,
+						authorize_url = authurl, 
+					)
+					
+					upload_start_time = int(re.search('uploadStartTimeInSeconds=(\d+)*',
+										callback_url).group(1))
 
-			upload_end_time = int(re.search('uploadEndTimeInSeconds=(\d+)*',
-								callback_url).group(1))
+					upload_end_time = int(re.search('uploadEndTimeInSeconds=(\d+)*',
+										callback_url).group(1))
 
-			callback_url = callback_url.split('?')[0]
+					callback_url = callback_url.split('?')[0]
 
-			data = {
-				'uploadStartTimeInSeconds': upload_start_time,
-				'uploadEndTimeInSeconds':upload_end_time
-			}
+					data = {
+						'uploadStartTimeInSeconds': upload_start_time,
+						'uploadEndTimeInSeconds':upload_end_time
+					}
 
-			sess = service.get_session((access_token, access_token_secret))
-			
-			r = sess.get(callback_url, header_auth=True, params=data)
-			
-			obj_list = _createObjectList(user, r.json(), dtype,upload_start_time)
+					sess = service.get_session((access_token, access_token_secret))
+					
+					r = sess.get(callback_url, header_auth=True, params=data)
+					
+					obj_list = _createObjectList(user, r.json(), dtype,upload_start_time)
 
-			MODEL_TYPES[dtype].objects.bulk_create(obj_list)
+					try:
+						MODEL_TYPES[dtype].objects.bulk_create(obj_list)
+						update_notification_state(ping_notif_obj,"processed")
+					except DatabaseError as e:
+						update_notification_state(ping_notif_obj,"failed")
 
-			# Call celery task to calculate/recalculate quick look for date to
-			# which received data belongs for the target user
-			date = _get_data_start_time(r.json(),dtype)
-			yesterday = datetime.now() - timedelta(days=1)
+					# Call celery task to calculate/recalculate quick look for date to
+					# which received data belongs for the target user
+					date = _get_data_start_time(r.json(),dtype)
+					yesterday = datetime.now() - timedelta(days=1)
 
-			if datetime.strptime(date,"%Y-%m-%d").date() == yesterday.date():
-				# if receive yesterday data then update the cumulative sums for yesterday
-				# as well.  
-				yesterday = yesterday.strftime("%Y-%m-%d")
-				chain = (
-					generate_quicklook.si(user.id,date,date)|
-				 	generate_cumulative_instances_custom_range.si(user.id,date,date)
-				)
-				chain.delay()
-			elif datetime.strptime(date,"%Y-%m-%d").date() != datetime.now().date():
-				# if received data is not for today (some historical data) then
-				# we have to update all the PA report from that date. So we need to record
-				# this date in database and update PA later as a celery task
-				generate_quicklook.delay(user.id,date,date)
-				set_pa_report_update_date.delay(
-					user.id, 
-					date
-				)
-			else:
-				generate_quicklook.delay(user.id,date,date)
+					if datetime.strptime(date,"%Y-%m-%d").date() == yesterday.date():
+						# if receive yesterday data then update the cumulative sums for yesterday
+						# as well.  
+						yesterday = yesterday.strftime("%Y-%m-%d")
+						chain = (
+							generate_quicklook.si(user.id,date,date)|
+						 	generate_cumulative_instances_custom_range.si(user.id,date,date)
+						)
+						chain.delay()
+					elif datetime.strptime(date,"%Y-%m-%d").date() != datetime.now().date():
+						# if received data is not for today (some historical data) then
+						# we have to update all the PA report from that date. So we need to record
+						# this date in database and update PA later as a celery task
+						generate_quicklook.delay(user.id,date,date)
+						set_pa_report_update_date.delay(
+							user.id, 
+							date
+						)
+					else:
+						generate_quicklook.delay(user.id,date,date)
+		else:
+			print('Summary type "{}" is not supported'.format(dtype))
