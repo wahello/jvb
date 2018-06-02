d3ce29844da911518b662064b4a219ba5d10d491
dks7inc <dileepk@s7inc.co>
2018-06-02 19:16:25 +0530

    changed HRR api

--- hrr/urls.py
+++ hrr/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^hrr_calculations_api$',views.hrr_calculations_api,
+	url(r'^hrr_calculations$',views.hrr_calculations,
 								  name="hrr"),
 	url(r'^aa_calculations$',views.aa_calculations,
 								  name="aa"),
