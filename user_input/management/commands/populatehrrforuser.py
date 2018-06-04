a0f9632d8daee0b3e810cc1acc253b1f1ba6780a
Atul Kumar <atulk@s7inc.co>
2018-06-04 15:21:24 +0530

    WIP: Completed populatehrrforuser management command. Have to test it

--- user_input/management/commands/populatehrrforuser.py
+++ user_input/management/commands/populatehrrforuser.py
@@ -4,7 +4,7 @@ import logging
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth import get_user_model
 
-from hrr.views import hrr_calculations
+from hrr.views import hrr_data
 
 logger = logging.getLogger(__name__)
 
@@ -12,14 +12,55 @@ def _str_to_datetime(str_date):
 	y,m,d = map(int,str_date.split('-'))
 	return datetime(y,m,d,0,0,0)
 
+def _sec_to_min_sec_str(seconds):
+	'''
+	Convert seconds to minute seconds string
+	
+	Args:
+		seconds (int): seconds
+
+	Examples:
+		>>> _str_to_datetime(140)
+		"2:20"
+	'''
+	if seconds:
+		mins,secs = divmod(seconds,60)
+		if secs < 10:
+			secs = "0{}".format(secs)
+		return "{}:{}".format(mins,secs)
+	return "0:00"
+
 def populate_userinput_hrr(user,from_date, to_date):
-	user_inputs_in_duration = user.user_input.filter(
+	'''
+	Populate older user inputs with calculated HRR data if not 
+	already submitted by the user
+
+	Args:
+		user (:obj: `User`): A User object
+		from_date (str): Date string representing start date from which update have to be done.
+			Expected format is YYYY-MM-DD. For example - "2018-06-01"
+		to_date (str): Date string representing end date upto which update have to be done.
+			Expected format is YYYY-MM-DD. For example - "2018-06-04"
+	'''
+	user_inputs_in_duration = user.userdailyinput_set.select_related().filter(
 		created_at__range = (from_date,to_date)
 	)
 	for user_input in user_inputs_in_duration:
 		try:
-			hrr_data = hrr_calculations(user,user_input.created_at)
-			# TODO : update the user input HRR fields with HRR data 
+			data = hrr_data(user,user_input.created_at)
+			measured_hrr = user_input.encouraged_input.measured_hrr
+			if (data['Did_you_measure_HRR'] == 'yes' and (not measured_hrr or measured_hrr == "no")):
+				if data['Did_you_measure_HRR']:
+					user_input.encouraged_input.measured_hr = data['Did_you_measure_HRR']
+				if data['Did_heartrate_reach_99']:
+					user_input.encouraged_input.hr_down_99 = data['Did_heartrate_reach_99']
+				if data['HRR_start_beat']:
+					user_input.encouraged_input.hr_level = str(data['HRR_start_beat'])
+				if data['lowest_hrr_1min']:
+					user_input.encouraged_input.lowest_hr_first_minute = str(data['lowest_hrr_1min'])
+				if data['time_99']:
+					user_input.encouraged_input.time_to_99 = _sec_to_min_sec_str(int(data['time_99']))
+				user_input.encouraged_input.save()
 		except Exception as e:
 			logger.exception("Cannot update user input for user {} on {}".format(
 					user.username,
@@ -27,7 +68,7 @@ def populate_userinput_hrr(user,from_date, to_date):
 				)
 			)
 
-class Commands(BaseCommand):
+class Command(BaseCommand):
 	help = "Populate existing user inputs with HRR data"
 
 	def __init__(self,*args, **kwargs):
@@ -68,7 +109,7 @@ class Commands(BaseCommand):
 
 		# give "all" flag more preference over "email" 
 		if options['all']:
-			self.email_or_all_flag= 'all'	
+			self.email_or_all_flag = 'all'	
 
 		return True
 
@@ -86,7 +127,7 @@ class Commands(BaseCommand):
 			populate_userinput_hrr(user,from_date,to_date)
 
 	def handle (self,*args,**options):
-		if self._validate_options():
+		if self._validate_options(options):
 			from_date = options['duration'][0]
 			to_date = options['duration'][1]
 
