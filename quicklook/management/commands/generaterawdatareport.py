404644796ced347fe6e522056b32c6a4f98caf93
Atul Kumar <atulk@s7inc.co>
2018-06-25 16:07:55 +0530

    Added support for username
    * Added new argument --username which will allow to generate
    raw data report for provided list of usernames.
    * Added usage of command
    * Refactored code

--- quicklook/management/commands/generaterawdatareport.py
+++ quicklook/management/commands/generaterawdatareport.py
@@ -7,10 +7,78 @@ from garmin.models import UserGarminDataSleep
 from quicklook.tasks import generate_quicklook
 
 class Command(BaseCommand):
+	'''
+	Management command to generate raw data report for users by
+	providing list of email address or usernames for provided 
+	time duration.
 
+	Example Usages:
+		1) Generate report for user(s) for current day using username.
+			A list of username is also supported for generating report for 
+			multiple user at once. Usernames should be separated by white space
+
+			$ python manage.py generaterawdatareport --username naruto copy-ninja
+
+		2) Generate report for user for current day using email address.
+			A list of email addresses is also supported for generating report
+			for multiple user at once. Addressed should be separated
+			by white space
+
+			$ python manage.py generaterawdatareport --email naruto@example.com \
+			copy_ninja@example.com
+
+		3) Generate report for every user in the database by providing
+		 	'--all' flag.
+
+			$ python manage.py generaterawdatareport --all
+
+		4) Generate report for user(s) for certain time duration. 
+
+			$ python manage.py generaterawdatareport --username naruto \
+			--duration 2018-06-01 2018-06-10
+
+			The duration argument takes two date string in YYYY-MM-DD format.
+			First date should be start date form which reports has to be
+			created and second should be end date up to which report has 
+			to be created.
+
+		5) Generate report from the date user have Garmin health data by
+			providing --origin or -o flag. So for example, if the user have
+			health data from April 1, 2018 then report following command
+			generate report from April 1, 2018 to current date.
+			
+			$ python manage.py generaterawdatareport --username naruto \
+			--origin
+
+		6) Similarly using --yesterday, --week, --month, --year flags reports
+			for yesterday, last 7 days, last 30 days and last 365 days can
+			be generated
+
+		7) Ignoring email address using --ignore or -i command. This takes a list
+			of email addresses and reports for those users will not be generated
+
+			$ python manage.py generaterawdatareport --all --origin \
+			--ignore copy_ninja@example.com
+
+	Note:
+		1) If any combination of these arguments --email, --username and
+			--all is provided in that case report will be generated on
+			the basis of following argument priority - 
+			--all > --username > --email 
+
+		2) Providing at least one of the following argument is necessary - 
+			--all, --email, --username
+
+		3) If any combination of following arguments is provided - 
+			--origin, --year, --month, --week, --yesterday, --duration
+			then report will be generated on the basis of following
+			argument priority - 
+			--origin > --year > --month > --week > --yesterday > --duration
+
+	'''
 	def __init__(self,*args, **kwargs):
 		super().__init__(*args, **kwargs)
-		self.email_or_all_flag = 'email'
+		self.accnt_slection_flag = 'email'
 		self.date_range_flag = 'today'
 
 	help = 'Generate Raw data report'
@@ -18,13 +86,16 @@ class Command(BaseCommand):
 	def _get_flags(self):
 		'''
 		Flags and their priority among group.
-		Each tuple have following meta data- (dest, short flag name, verbose flag name, priority) 
-		Group 1 = [email, all]
+		Each tuple have following meta data- (dest, short flag name, 
+			verbose flag name, priority) 
+
+		Group 1 = [email, username, all]
 		Group 2 = [duration, yesterday, week, month, year]
 		Lower priority value is given more preference
 		'''
 		flags = {
-			'email':('email','-e','--email',2),
+			'email':('email','-e','--email',3),
+			'username':('username','-u','--username',2),
 			'all':('all','-a','--all',1),
 			'duration':('duration','-d','--duration',6),
 			'yesterday':('yesterday','-y','--yesterday',5),
@@ -36,6 +107,10 @@ class Command(BaseCommand):
 		return flags
 
 	def _get_origin_date(self, user):
+		'''
+		Get the date of the oldest sleep summaries a user have. This gives the 
+		rough idea from when user have health API data. 
+		'''
 		last_record = UserGarminDataSleep.objects.filter(user = user).order_by('id')
 		if last_record.exists():
 			start_time = datetime.utcfromtimestamp(last_record[0].start_time_in_seconds)
@@ -44,6 +119,9 @@ class Command(BaseCommand):
 			return None
 
 	def _validate_options(self,options):
+		'''
+		Validate the provided options
+		'''
 		no_flags = True
 		flags = self._get_flags()
 
@@ -55,20 +133,26 @@ class Command(BaseCommand):
 		if no_flags:
 			raise CommandError("No arguments are provided. Type -h or --help for more information")
 
-		if not options['email'] and not options['all']:
-			raise CommandError("Provide either --email or --all")
-
-		# give "all" flag more preference over "email" 
+		if not options['email'] and not options['all'] and not options['username']:
+			raise CommandError("Provide either --email or --username or --all")
+ 
 		if options['all']:
-			self.email_or_all_flag = 'all'	
+			# give "all" flag more preference over anything
+			self.accnt_slection_flag = 'all' 
+		elif options['username']:
+			# give "username" flag more preference over "email"
+			self.accnt_slection_flag = 'username'	
 
 
 		# if more than one flag  from Group 2 is provided then
 		# use flags with most priority (lowest value) 
 		flags.pop('email')
 		flags.pop('all')
+		flags.pop('username')
+
 		common_flags = set([f for f in flags.keys()]).intersection(
-					   set(list(filter(lambda x:options[x],[o for o in options.keys()]))))
+			set(list(filter(lambda x:options[x],[o for o in options.keys()])))
+		)
 		
 		for f in common_flags:
 			if self.date_range_flag and self.date_range_flag != 'today':
@@ -88,6 +172,15 @@ class Command(BaseCommand):
 			type = str,
 			dest = flags.get('email')[0],
 			help = "Email(s)"
+		)
+
+		parser.add_argument(
+			flags.get('username')[2],
+			flags.get('username')[1],
+			nargs = '+',
+			type = str,
+			dest = flags.get('username')[0],
+			help = "Username(s)"
 		)
 
 		parser.add_argument(
@@ -156,64 +249,86 @@ class Command(BaseCommand):
 			help = 'Email(s) to ignore'
 		)
 
+	def _generate_raw_data_reports(self,user_qs,options,from_date,to_date):
+		'''
+		Generated reports for users from 'from_date' to 'to_date'
+
+		Args:
+			user_qs (`obj`: Queryset): A queryset of users
+			options: Dictionary of Command line arguments
+			from_date (string): Start date from which report has to be created
+			to_date (string): End date up to which report has to be created 
+		'''
+		for user in user_qs:
+			if (not options['ignore'] 
+				or (options['ignore'] and user.email not in options['ignore'])): 
+				self.stdout.write(self.style.WARNING(
+					'\nCreating Raw data report for user "%s"' % user.username)
+				)
+				if self.date_range_flag == 'origin':
+					date_of_oldest_record = self._get_origin_date(user)
+					if date_of_oldest_record:
+						from_date = date_of_oldest_record.strftime("%Y-%m-%d")
+						to_date = datetime.now().strftime("%Y-%m-%d")
+						generate_quicklook(user.id,from_date,to_date)
+					else:
+						self.stdout.write(self.style.ERROR(
+							'\nNo health record found for user "%s"' % user.username)
+						)
+					continue
+				generate_quicklook(user.id,from_date,to_date)
+
+	def _get_update_date_range(self,options):
+		'''
+		Return 'from_date' and 'to_date' based on provided flag
+		'''
+		today = datetime.now().date()
+		yesterday = today-timedelta(days=1)
+		from_date = datetime.now().strftime("%Y-%m-%d")
+		to_date = datetime.now().strftime("%Y-%m-%d")
+
+		if self.date_range_flag == 'duration':
+			to_date = options['duration'][1]
+			from_date = options['duration'][0]
+
+		elif self.date_range_flag == 'yesterday':
+			to_date = yesterday.strftime("%Y-%m-%d")
+			from_date = yesterday.strftime("%Y-%m-%d")
+
+		elif self.date_range_flag == 'week':
+			td = timedelta(days=7)
+			to_date = yesterday.strftime("%Y-%m-%d")
+			from_date = (today-td).strftime("%Y-%m-%d")
+
+		elif self.date_range_flag == 'month':
+			td = timedelta(days=30)
+			to_date =yesterday.strftime("%Y-%m-%d")
+			from_date = (today-td).strftime("%Y-%m-%d")
+
+		elif self.date_range_flag == 'year':
+			td = timedelta(days=365)
+			to_date = yesterday.strftime("%Y-%m-%d")
+			from_date = (today-td).strftime("%Y-%m-%d")
+
+		return (from_date,to_date)
+
 	def handle(self, *args, **options):
 		if self._validate_options(options):
-			today = datetime.now().date()
-			yesterday = today-timedelta(days=1)
-			from_date = datetime.now().strftime("%Y-%m-%d")
-			to_date = datetime.now().strftime("%Y-%m-%d")
-
-			if self.date_range_flag == 'duration':
-				to_date = options['duration'][1]
-				from_date = options['duration'][0]
-
-			elif self.date_range_flag == 'yesterday':
-				to_date = yesterday.strftime("%Y-%m-%d")
-				from_date = yesterday.strftime("%Y-%m-%d")
-
-			elif self.date_range_flag == 'week':
-				td = timedelta(days=7)
-				to_date = yesterday.strftime("%Y-%m-%d")
-				from_date = (today-td).strftime("%Y-%m-%d")
-
-			elif self.date_range_flag == 'month':
-				td = timedelta(days=30)
-				to_date =yesterday.strftime("%Y-%m-%d")
-				from_date = (today-td).strftime("%Y-%m-%d")
-
-			elif self.date_range_flag == 'year':
-				td = timedelta(days=365)
-				to_date = yesterday.strftime("%Y-%m-%d")
-				from_date = (today-td).strftime("%Y-%m-%d")
-
-			if self.email_or_all_flag == 'email':
+			from_date,to_date = self._get_update_date_range(options)
+			if self.accnt_slection_flag == 'email':
 				emails = [e for e in options['email']]
 				user_qs = User.objects.filter(email__in = emails)
-				for user in user_qs:
-					if not options['ignore'] or (options['ignore'] and user.email not in options['ignore']): 
-						self.stdout.write(self.style.WARNING('\nCreating Raw data report for user "%s"' % user.username))
-						if self.date_range_flag == 'origin':
-							date_of_oldest_record = self._get_origin_date(user)
-							if date_of_oldest_record:
-								from_date = date_of_oldest_record.strftime("%Y-%m-%d")
-								to_date = datetime.now().strftime("%Y-%m-%d")
-								generate_quicklook(user.id,from_date,to_date)
-							else:
-								self.stdout.write(self.style.ERROR('\nNo health record found for user "%s"' % user.username))
-							continue
-						generate_quicklook(user.id,from_date,to_date)
+				self._generate_raw_data_reports(
+					user_qs, options, from_date, to_date
+				)
+			elif self.accnt_slection_flag == 'username':
+				usernames = [username for username in options['username']]
+				user_qs = User.objects.filter(username__in = usernames)
+				self._generate_raw_data_reports(
+					user_qs, options, from_date, to_date
+				)
 			else:
 				user_qs = User.objects.all()
-				for user in user_qs:
-					if not options['ignore'] or (options['ignore'] and user.email not in options['ignore']): 
-						self.stdout.write(self.style.WARNING('\nCreating Raw data report for user "%s"' % user.username))
-						if self.date_range_flag == 'origin':
-							date_of_oldest_record = self._get_origin_date(user)
-							if date_of_oldest_record:
-								from_date = date_of_oldest_record.strftime("%Y-%m-%d")
-								to_date = datetime.now().strftime("%Y-%m-%d")
-								generate_quicklook(user.id,from_date,to_date)
-							else:
-								self.stdout.write(self.style.ERROR('\nNo health record found for user "%s"' % user.username))
-							continue
-						generate_quicklook(user.id,from_date,to_date)
+				self._generate_raw_data_reports(user_qs,
+					options,from_date,to_date
+				)
