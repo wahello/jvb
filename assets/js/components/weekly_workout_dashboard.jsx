57431da707fd952f5baba3d4eda1f72141b43030
Suresh K <suresh@s7inc.co>
2018-07-16 17:10:40 +0530

    added calendar,forward arrow and backward arrow

--- assets/js/components/weekly_workout_dashboard.jsx
+++ assets/js/components/weekly_workout_dashboard.jsx
@@ -11,6 +11,7 @@ import { Collapse, Navbar, NavbarToggler,
          NavbarBrand, Nav, NavItem, NavLink,
         Button,Popover,PopoverBody,Form,FormGroup,FormText,Label,Input} from 'reactstrap';
 import NavbarMenu from './navbar';
+import {fetchWeeklyWorkoutData} from '../network/weekly_workout';
 
 
 axiosRetry(axios, { retries: 3});
@@ -28,8 +29,18 @@ class WorkoutDashboard extends Component{
 	    this.renderAddDate = this.renderAddDate.bind(this);
 		this.renderRemoveDate = this.renderRemoveDate.bind(this);
 		this.toggleCalendar = this.toggleCalendar.bind(this);
+		this.successWeeklyWorkoutData = this.successWeeklyWorkoutData.bind(this);
+		this.errorWeeklyWorkoutData = this.errorWeeklyWorkoutData.bind(this);
+		this.processDate = this.processDate.bind(this);
 	}
+	successWeeklyWorkoutData(data){
+		this.setState({
 
+		})
+  	}
+  	errorWeeklyWorkoutData(error){
+		console.log(error.message);
+  	}
 	renderAddDate(){
 		/*It is forward arrow button for the calender getting the next day date*/
 		var today = this.state.selectedDate;
@@ -37,7 +48,7 @@ class WorkoutDashboard extends Component{
 		this.setState({
 			selectedDate:tomorrow.toDate()
 		},()=>{
-			fetchMovementData(this.successMovementData,this.errorMovementData,this.state.selectedDate);
+			//fetchWeeklyWorkoutData(this.successWeeklyWorkoutData,this.errorWeeklyWorkoutData,this.state.selectedDate);
 		});
 	}
 	renderRemoveDate(){
@@ -47,7 +58,7 @@ class WorkoutDashboard extends Component{
 		this.setState({
 			selectedDate:tomorrow.toDate()
 		},()=>{
-			fetchMovementData(this.successMovementData,this.errorMovementData,this.state.selectedDate);
+			//fetchWeeklyWorkoutData(this.successWeeklyWorkoutData,this.errorWeeklyWorkoutData,this.state.selectedDate);
 		});
 	}
 	toggleCalendar(){
@@ -56,13 +67,53 @@ class WorkoutDashboard extends Component{
 	      calendarOpen:!this.state.calendarOpen
 	    });
     }
+     processDate(selectedDate){
+		this.setState({
+			selectedDate:selectedDate,
+			calendarOpen:!this.state.calendarOpen,
+		},()=>{
+			fetchWeeklyWorkoutData(this.successWeeklyWorkoutData,this.errorWeeklyWorkoutData,this.state.selectedDate);
+		});
+	}
 	render(){
 		return(
-				<div>
+				<div className = "container-fluid">
 					<NavbarMenu title = {<span style = {{fontSize:"22px"}}>
 					Weekly Workout Summary Report
 					</span>} />
 
+					<div className = "cla_center">
+						<span>
+							<span onClick = {this.renderRemoveDate} style = {{marginLeft:"30px",marginRight:"14px"}}>
+								<FontAwesome
+			                        name = "angle-left"
+			                        size = "2x"
+				                />
+							</span> 
+			            	<span id="navlink" onClick={this.toggleCalendar} id="gd_progress">
+			                    <FontAwesome
+			                        name = "calendar"
+			                        size = "2x"
+			                    />
+			                    <span style = {{marginLeft:"20px",fontWeight:"bold",paddingTop:"7px"}}>{moment(this.state.selectedDate).format('MMM DD, YYYY')}</span>  
+		                	</span>
+		                	<span onClick = {this.renderAddDate} style = {{marginLeft:"14px"}}>
+								<FontAwesome
+			                        name = "angle-right"
+			                        size = "2x"
+				                />
+							</span> 
+			            	<Popover
+					            placement="bottom"
+					            isOpen={this.state.calendarOpen}
+					            target="gd_progress"
+					            toggle={this.toggleCalendar}>
+				                <PopoverBody className="calendar2">
+				                <CalendarWidget  onDaySelect={this.processDate}/>
+				                </PopoverBody>
+			                </Popover>
+	                	</span>
+		        	</div>
 					 <div className = "row justify-content-center hr_table_padd">
 							<div className = "table table-responsive">
 				          	    <table className = "table table-striped table-bordered ">
