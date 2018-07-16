5fbd4c81591c07c69383aba8e21961f63969ae7e
Suresh K <suresh@s7inc.co>
2018-07-16 15:49:33 +0530

    added comma for non exercise steps in my ranking page

--- assets/js/components/leader_exp.jsx
+++ assets/js/components/leader_exp.jsx
@@ -513,6 +513,20 @@ class LeaderBoard1 extends Component{
 	                scoreTableData.push(<td className="lb_table_style_rows">{x1 + x2}</td>);
     	        }
 	  		}
+	  		else if(category == "Non Exercise Steps"){
+	  			var value = score;
+	  		    if(value != undefined){
+	                value += '';
+	                var x = value.split('.');
+	                var x1 = x[0];
+	                var x2 = x.length > 1 ? '.' + x[1] : '';
+	                var rgx = /(\d+)(\d{3})/;
+	                while (rgx.test(x1)) {
+	            			x1 = x1.replace(rgx, '$1' + ',' + '$2');
+	          		}
+	                scoreTableData.push(<td className="lb_table_style_rows">{x1 + x2}</td>);
+    	        }
+	  		}
 	  		else if(category == "Percent Unprocessed Food"){
 	  			scoreTableData.push(null);
 	  		}
