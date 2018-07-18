6b24973cbc0706bafd0e8da89ed48dfe2e25fb6d
dks7inc <dileepk@s7inc.co>
2018-07-18 13:31:43 +0530

    Added another url for hrr

--- hrr/urls.py
+++ hrr/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	# url(r'^hrr_calculations$',views.hrr_calculations,
-	# 							  name="hrr"),
+	url(r'^refresh/hrr_calculations$',views.hrr_calculations,
+								  name="hrr"),
 	url(r'^aa_calculations$',views.aa_calculations,
 								  name="aa"),
 	url(r'^aa_workout_calculations$',views.aa_workout_calculations,
