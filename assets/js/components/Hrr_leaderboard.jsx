d7036e1b90d736158675141d5d6e8895a1860e0e
Suresh K <suresh@s7inc.co>
2018-07-25 13:28:09 +0530

    added color schema for heartbeats

--- assets/js/components/Hrr_leaderboard.jsx
+++ assets/js/components/Hrr_leaderboard.jsx
@@ -17,8 +17,28 @@ class HrrLeaderboard extends Component{
 	    this.state = {
 	    }
 		this.renderTable = this.renderTable.bind(this);
+		this.heartBeatsColors = this.heartBeatsColors.bind(this);
 	}
-	
+	heartBeatsColors(value){
+  		/* Applying the colors for the table cells depends upon their heart beat ranges*/
+  		let background = "";
+  		let color = "";
+  		if(value && value != "N/A"){
+	  		if(value >= 20){
+	  			background = "green";
+	  			color = "white";
+	  		}
+	  		else if(value >= 12 && value < 20){
+	  			background = "yellow";
+	  			color = "black";
+	  		}
+	  		else if(value > 0 && value < 12){
+	  			background = "red";
+	  			color = "black";
+	  		}
+  		}
+  		return <td style ={{background:background,color:color}}>{value}</td>
+  	}
 	renderTable(Hrr_data){
 		let td_rows = [];
 		let keys = ["rank","username","time_99","beat_lowered","pure_time_99","pure_beat_lowered","total_hrr_rank_point"];
@@ -27,6 +47,16 @@ class HrrLeaderboard extends Component{
 			for(let key1 of keys){
 				if(key1 == "rank" || key1 == "username" || key1 == "total_hrr_rank_point"){
 					td_values.push(<td>{value[key1]}</td>);
+				}
+				else if(key1 == "beat_lowered" || key1 == "pure_beat_lowered"){
+					for(let [key3,value4] of Object.entries(value[key1])){
+						if(key3 == "rank"){
+							td_values.push(<td>{value4}</td>);
+						}
+						else if(key3 == "score"){
+							td_values.push(this.heartBeatsColors(value4.value));
+						}
+					}
 				}
 				else{
 					for(let [key3,value4] of Object.entries(value[key1])){
