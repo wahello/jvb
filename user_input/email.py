222ea2b0f9dd807dbdb9a583fcadbf0b957de2a1
dks7inc <dileepk@s7works.io>
2018-09-25 18:03:40 +0530

    remind user through mail to synchronise watch

--- user_input/email.py
+++ user_input/email.py
@@ -36,3 +36,121 @@ JVB Health & Wellness
 			recipient_list = admin_users_email,
 			fail_silently = True  
 		)
+
+
+
+# Reminding Users to Synchronize watch
+def remind_sync_user_watch_shift1():
+
+	feed_back_email="info@jvbwellness.com"
+	date_today=date.today()
+	all_users=User.objects.all()
+	users_sync_data=UserLastSynced.objects.filter(Q(last_synced__gte=datetime(
+		date_today.year,date_today.month,date_today.day,0,0,0))&
+		Q(last_synced__lte=datetime.now()))
+	
+	users_sync_name=[users_sync_data[name].user for name in range(len(users_sync_data))]
+	
+	for user in range(len(all_users)):
+		users_sync_data_last=UserLastSynced.objects.filter(user=all_users[user]).last()
+		
+		if not all_users[user] in users_sync_name:
+			if users_sync_data_last:
+				remind_users_sync_emails=[all_users[user].email]
+				message="""
+Hi {},
+
+We noticed that you have not synchronized your watch today {}, your last synchronisation on {} at {}.
+
+Thanks and let us know if you have any questions by emailing mailto:{} 
+
+Sincerely,
+JVB Health and Wellness
+				"""
+				message=message.format(all_users[user].username,datetime.today(),users_sync_data_last.last_synced.date(),users_sync_data_last.last_synced.time(),feed_back_email)
+				if remind_users_sync_emails:
+					send_mail(
+					subject="Reminder To Synchronize Watch" ,
+					message = message,
+					from_email = "info@jvbwellness.com",
+					recipient_list = remind_users_sync_emails,
+					fail_silently = True  
+					)
+			else:
+				message= """
+Hi {},
+
+We noticed that you have not started to synchronize your watch.
+
+Thanks and let us know if you have any questions by emailing mailto:{} 
+
+Sincerely,
+JVB Health and Wellness
+				"""
+				remind_users_email=[all_users[user].email]
+				message=message.format(all_users[user].username,feed_back_email)
+				if remind_users_email:
+					send_mail(
+						subject="Reminder To Synchronize Watch" ,
+						message = message,
+						from_email = "info@jvbwellness.com",
+						recipient_list = remind_users_email,
+						fail_silently = True
+						)
+
+def remind_sync_user_watch_shift2():
+
+	feed_back_email="info@jvbwellness.com"
+	date_today=date.today()
+	all_users=User.objects.all()
+	users_sync_data=UserLastSynced.objects.filter(Q(last_synced__gte=datetime(
+		date_today.year,date_today.month,date_today.day,9,0,0))&
+		Q(last_synced__lte=datetime.now()))
+	
+	users_sync_name=[users_sync_data[name].user for name in range(len(users_sync_data))]
+	
+	for user in range(len(all_users)):
+		users_sync_data_last=UserLastSynced.objects.filter(user=all_users[user]).last()
+		if not all_users[user] in users_sync_name:
+			if users_sync_data_last:
+				remind_users_sync_emails=[all_users[user].email]
+				message="""
+Hi {},
+
+We noticed that you have not synchronized your watch today {}, your last synchronisation on {} at {}.
+
+Thanks and let us know if you have any questions by emailing mailto:{} 
+
+Sincerely,
+JVB Health and Wellness
+				"""
+				message=message.format(all_users[user].username,date.today(),users_sync_data_last.last_synced.date(),users_sync_data_last.last_synced.time(),feed_back_email)
+				if remind_users_sync_emails:
+					send_mail(
+					subject="Reminder To Synchronize Watch" ,
+					message = message,
+					from_email = "info@jvbwellness.com",
+					recipient_list = remind_users_sync_emails,
+					fail_silently = True  
+					)
+			else:
+				message= """
+Hi {},
+
+We noticed that you have not started to synchronize your watch.
+
+Thanks and let us know if you have any questions by emailing mailto:{} 
+
+Sincerely,
+JVB Health and Wellness
+				"""
+				remind_users_sync_email=[all_users[user].email]
+				message=message.format(all_users[user].username,feed_back_email)
+				if remind_users_sync_email:
+					send_mail(
+						subject="Reminder To Synchronize Watch" ,
+						message = message,
+						from_email = "info@jvbwellness.com",
+						recipient_list = remind_users_email,
+						fail_silently = True
+						)
