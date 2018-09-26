56598ab03b5668575b18b8e6d8e6e2dcb8b9ed4d
venkatesh25 <venkatesh@s7works.io>
2018-09-26 21:49:19 +0530

    Renamed the task

--- user_input/tasks.py
+++ user_input/tasks.py
@@ -2,8 +2,7 @@ from celery.decorators import task
 from celery.utils.log import get_task_logger
 
 from .email import (send_userinput_update_email,
-		remind_sync_user_watch_shift1,remind_sync_user_watch_shift2,
-		remind_selected_users_submit_user_input)#user_inputs_remind_emails,)
+	notify_user_to_submit_userinputs)
 
 
 
@@ -22,52 +21,14 @@ def notify_admins_task(admin_users_email,instance_meta):
 		logger.error(str(e),exc_info=True)
 
 
-@task(name="remind_selected_users.submit_user__daily_input")
+@task(name="userinputs.submit_userinput_reminder")
 def remind_selected_users_submit_input():
 	'''
-		Celery task to send email to Synchronize watch for before 9:00 PM
+		Celery task to send email to users to submit user
+		input form at 10:00 pm NY time
 	'''
 	try:
-		remind_selected_users_submit_user_input()
-		logger.info("Sent email successfully")
+		notify_user_to_submit_userinputs()
+		logger.info("Sent email to notify user to submit user inputs successfully")
 	except Exception as e:
 		logger.error(str(e),exc_info=True)
-
-
-# @task(name="userinputs.remind_email")
-# def remind_user_inputs_email():
-# 	'''
-# 		Celery task to send email to submits/updates user inputs
-# 	'''
-# 	try:
-# 		user_inputs_remind_emails()
-# 		logger.info("Sent email successfully")
-# 	except Exception as e:
-# 		logger.error(str(e),exc_info=True)
-
-
-
-@task(name="remind_users_sync_watch_shift1")
-def remind_users_sync_watch_day():
-	'''
-		Celery task to send email to Synchronize watch for before 9:00 AM
-	'''
-	try:
-		remind_sync_user_watch_shift1()
-		logger.info("Sent email successfully")
-	except Exception as e:
-		logger.error(str(e),exc_info=True)
-
-@task(name="remind_users_sync_watch_shift2")
-def remind_users_sync_watch_night():
-	'''
-		Celery task to send email to Synchronize watch for before 9:00 PM
-	'''
-	try:
-		remind_sync_user_watch_shift2()
-		logger.info("Sent email successfully")
-	except Exception as e:
-		logger.error(str(e),exc_info=True)
-
-
-
