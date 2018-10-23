1b679ddb5dcd949d6d98913b18f268ad680322fb
dks7inc <dileepk@s7inc.co>
2018-10-23 17:58:09 +0530

    Changed fitfile created at from auto_not to auto_now_add

--- garmin/models.py
+++ garmin/models.py
@@ -123,7 +123,7 @@ class GarminConnectToken(models.Model):
 
 class GarminFitFiles(models.Model):
 	user = models.ForeignKey('auth.user', on_delete=models.CASCADE, related_name="garmin_fit_files")
-	created_at = models.DateTimeField(auto_now=True)
+	created_at = models.DateTimeField(auto_now_add=True)
 	fit_file_belong_date = models.DateField(default=None,null=True)
 	fit_file = models.BinaryField(blank=True)
 	meta_data_fitfile = models.TextField()
