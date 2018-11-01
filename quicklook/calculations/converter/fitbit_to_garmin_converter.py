a59b2c23027e87e84af66279b5279ee92807b149
ananyas7 <ananya@s7works.io>
2018-11-01 12:36:20 +0530

    commented the steps line in fitbit_to_garmin_activities

--- quicklook/calculations/converter/fitbit_to_garmin_converter.py
+++ quicklook/calculations/converter/fitbit_to_garmin_converter.py
@@ -169,7 +169,7 @@ def fitbit_to_garmin_activities(active_summary):
 		# garmin_activites['maxPaceInMinutesPerKilometer'] = active_summary.get("")
 		# garmin_activites['maxRunCadenceInStepsPerMinute'] = active_summary.get("")
 		# garmin_activites['maxSpeedInMetersPerSecond'] = active_summary.get("")
-		garmin_activites['steps'] = active_summary.get('steps',0)
+		# garmin_activites['steps'] = active_summary.get('steps',0)
 		# garmin_activites['totalElevationGainInMeters'] = active_summary.get("")
 		# garmin_activites['totalElevationLossInMeters'] = active_summary.get("")
 		# garmin_activites['resting_hr_last_night'] = active_summary.get("")
