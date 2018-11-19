8392d2c6938bcb5c3b9814747cbc43018dd7cba3
Atul Kumar <atulk@s7inc.co>
2018-11-19 17:49:13 +0530

    Rewired n/w calls to use new API for device tokens availability
    Updated the ajax call to use new API endpoint for device token availability. Also
    updated the logic such that if user has linked atleast any one of the supported
    wearable devices then pop up for linking other devices will not appear.

--- assets/js/components/dashboard/Dashboard.jsx
+++ assets/js/components/dashboard/Dashboard.jsx
@@ -3,119 +3,76 @@ import { connect } from 'react-redux';
 import { Link,withRouter } from 'react-router-dom';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import NavbarMenu from '../navbar';
-import {haveGarminToken,haveFitBitToken} from "../../network/dashboard";
+import {haveDeviceToken} from "../../network/dashboard";
+
 class Dashboard extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			 modal: false,
-			"have_garmin_connect_token":"",
-    		"have_garmin_health_token":"",
-    		"have_fitbit_tokens":"",
+			 linked_devices:true,
+			 have_garmin_connect_token:false,
+    		 have_garmin_health_token:false,
+    		 have_fitbit_token:false,
 		};
-		this.noGarminTokenModel = this.noGarminTokenModel.bind(this);
+		this.noDeviceLinkedModel = this.noDeviceLinkedModel.bind(this);
 		this.successToken = this.successToken.bind(this);
 		this.errorToken = this.errorToken.bind(this);
-		this.garminHealthModel = this.garminHealthModel.bind(this);
-		this.garminConnectModel = this.garminConnectModel.bind(this);
 		this.toggle = this.toggle.bind(this);
-		this.successFitBitToken = this.successFitBitToken.bind(this);
-		this.errorFitBitToken = this.errorFitBitToken.bind(this);
-		//this.fitBitTokenModel = this.fitBitTokenModel.bind(this);
 
 	}
 
 	successToken(data){
 		this.setState({
-			modal:true,
+			linked_devices:data.data.linked_devices,
 			have_garmin_connect_token:data.data.have_garmin_connect_token,
 			have_garmin_health_token:data.data.have_garmin_health_token,
+			have_fitbit_token:data.data.have_fitbit_token
 		});
 	}
-	successFitBitToken(data){
-		
-		this.setState({
-			modal:true,
-			have_fitbit_tokens:data.data.have_fitbit_tokens
-		});
-	}
-	toggle() {
-	    this.setState({
-	      modal: !this.state.modal
-	    });
-  	}
-
-  	/* If we don't have two garmin tokents and even single tokent below model functions will be work*/
-	noGarminTokenModel(){
-			let modal =  <Modal isOpen={this.state.modal} toggle = {this.toggle} className={this.props.className}>
-					          <ModalHeader>Connect Garmin Accounts</ModalHeader>
-					          <ModalBody>
-					            	We noticed that you have not linked your Garmin Health account and Garmin Connect account to us yet.
-					            	Click the links below to link your account so we can provide you with lots of cool reporting, grades from your data and analysis from your workouts!
-					          </ModalBody>
-					          <ModalFooter>
-					          	    <a href='/users/request_token' className = "garminlink"><Button color="primary" style = {{fontSize:"13px"}}>Link Garmin Health Data<br/></Button></a>
-									<a href='/users/connect_request_token' className = "garminlink"><Button color="primary" style = {{fontSize:"13px"}}> Link Garmin Connect Data<br/></Button></a>
-					            	<Button color="primary" onClick={this.toggle} style = {{fontSize:"13px"}}>Ok</Button>
-					          </ModalFooter>
-				        </Modal>
-	        return modal;
-	}
-
-	garminHealthModel(){
-		let modal =  <Modal isOpen={this.state.modal} toggle = {this.toggle} className={this.props.className}>
-				          <ModalHeader>Garmin Health Connect</ModalHeader>
-				          <ModalBody>
-				            	We noticed that you have not linked your Garmin Health account to us yet.
-				            	Click the link below to link your account so we can provide you with lots of cool reporting and grades from your data!
-				          </ModalBody>
-				          <ModalFooter>
-				          		<a href='/users/request_token' className = "garminlink"><Button color="primary"style = {{fontSize:"13px"}} >Link Garmin Health Data<br/></Button></a>
-				            	<Button color="primary" onClick={this.toggle} style = {{fontSize:"13px"}}>Ok</Button>
-				          </ModalFooter>
-			        </Modal>
-        return modal;
-	}
-
-	garminConnectModel(){
-		let modal =  <Modal isOpen={this.state.modal} toggle = {this.toggle} className={this.props.className}>
-				          <ModalHeader>Garmin Connect</ModalHeader>
-				          <ModalBody>
-				          We noticed that you have not linked your Garmin Connect account to us yet.  
-				          Click the link below to link your account so we can provide you with lots of cool data and analysis from your workouts!
-				          </ModalBody>
-				          <ModalFooter>
-				          		<a href='/users/connect_request_token' className = "garminlink"><Button color="primary" style = {{fontSize:"13px"}}> Link Garmin Connect Data<br/></Button></a>
-				            	<Button color="primary" onClick={this.toggle} style = {{fontSize:"13px"}}>Ok</Button>
-				          </ModalFooter>
-			        </Modal>
-        return modal;
-	}
-
-	/*fitBitTokenModel(){
-		let modal =  <Modal isOpen={this.state.modal} toggle = {this.toggle} className={this.props.className}>
-				          <ModalHeader>Fitbit Connect</ModalHeader>
-				          <ModalBody>
-				            	We noticed that you have not linked your FitBit account to us yet.
-				            	Click the link below to link your account so we can provide you with lots of cool reporting and grades from your data!
-				          </ModalBody>
-				          <ModalFooter>
-				          		<a href='/fitbit/request_token_fitbit' className = "garminlink"><Button color="primary"style = {{fontSize:"13px"}} >Connect to Fitbit<br/></Button></a>
-				            	<Button color="primary" onClick={this.toggle} style = {{fontSize:"13px"}}>Ok</Button>
-				          </ModalFooter>
-			        </Modal>
-        return modal;
-	}*/
 
 	errorToken(error){
 		console.log(error.message);
 	}
-	errorFitBitToken(error){
-		console.log(error.message);
+
+	toggle() {
+	    this.setState({
+	      linked_devices: !this.state.linked_devices
+	    });
+  	}
+
+  	/* If user haven't linked any device yet, then show this popup to remind user to link atleast on device*/
+	noDeviceLinkedModel(){
+		let modal =  <Modal isOpen={!this.state.linked_devices} 
+						  toggle = {this.toggle} 
+						  className={this.props.className}>
+				          <ModalHeader>Link wearable device(s)</ModalHeader>
+				          <ModalBody>
+				            	We noticed that you have not linked any wearable device yet.
+				            	Click the links below to link devices supported by us
+				            	so we can provide you with lots of cool reporting, grades from your
+				            	data and analysis from your workouts!
+				          </ModalBody>
+				          <ModalFooter>
+				          	    <a href='/users/request_token' 
+				          	    	className = "garminlink">
+				          	    	<Button color="primary" style = {{fontSize:"13px"}}>Garmin Health<br/></Button>
+				          	    </a>
+								<a href='/users/connect_request_token' 
+									className = "garminlink">
+									<Button color="primary" style = {{fontSize:"13px"}}>Garmin Connect<br/></Button>
+								</a>
+								<a href='/fitbit/request_token_fitbit' 
+									className = "garminlink">
+									<Button color="primary" style = {{fontSize:"13px"}}>Fitbit<br/></Button>
+								</a>
+				            	<Button color="primary" onClick={this.toggle} style = {{fontSize:"13px"}}>OK</Button>
+				          </ModalFooter>
+			        </Modal>
+        return modal;
 	}
+
 	componentDidMount(){
-		haveGarminToken(this.successToken,this.errorToken);
-		haveFitBitToken(this.successFitBitToken,this.errorFitBitToken);
+		haveDeviceToken(this.successToken,this.errorToken);
 	}
 
 	render(){
@@ -152,7 +109,7 @@ class Dashboard extends Component {
 							  <Link to='/leaderboard'>My Rankings</Link><br/>
 							   <Link to='/heartrate'>Heartrate Aerobic/Anaerobic Ranges</Link><br/>
 							   
-							   <Link to='/heartrate_zone'>Time in Heart -Rate Zones Chart</Link><br/>
+							   <Link to='/heartrate_zone'>Time in Heart-Rate Zones Chart</Link><br/>
 							   
 
 							   {/*<Link to='/workout_stats'>Heartrate Workout</Link><br/>*/}
@@ -163,33 +120,33 @@ class Dashboard extends Component {
 							  <Link to='/rawdata#grades'>Grades</Link><br/>
 							  <a target="_blank" href = "/static/quicklook/grades_key.pdf">Grades Key</a><br/>
 							  {/*<Link to='/movement_consistency'>movement Consistency</Link><br/>*/}
-							{(this.state.have_garmin_connect_token == false && this.state.have_garmin_health_token == false) &&
-							  <div>
-								  <h3 id="link_style">Set up Links to Garmin</h3>
-								  <a href='/users/request_token'>Garmin Health Connect</a><br/>
-								  <a href='/users/connect_request_token'>Garmin Connect</a><br/>
-								  {this.noGarminTokenModel()}
-							  </div>
+							{(!this.state.have_garmin_connect_token 
+								&& !this.state.have_garmin_health_token)
+								&& <div>
+									  <h3 id="link_style">Set up Links to Garmin</h3>
+									  <a href='/users/request_token'>Garmin Health Connect</a><br/>
+									  <a href='/users/connect_request_token'>Garmin Connect</a><br/>
+							  	   </div>
 							}
-							{(this.state.have_garmin_connect_token == true && this.state.have_garmin_health_token == false) &&
-							  <div>
-								  <h3 id="link_style">Set up Links to Garmin</h3>
-								  <a href='/users/request_token'>Garmin Health Connect</a><br/>
-								  {this.garminHealthModel()}
-							  </div>
+							{(this.state.have_garmin_connect_token 
+								&& !this.state.have_garmin_health_token)
+								&& <div>
+									  <h3 id="link_style">Set up Links to Garmin</h3>
+									  <a href='/users/request_token'>Garmin Health Connect</a><br/>
+							  	   </div>
 							}
-							{(this.state.have_garmin_connect_token == false && this.state.have_garmin_health_token == true) &&
-							  <div>
-								  <h3 id="link_style">Set up Links to Garmin</h3>
-								  <a href='/users/connect_request_token'>Garmin Connect</a><br/>
-								  {this.garminConnectModel()}
-							  </div>
+							{(!this.state.have_garmin_connect_token 
+								&& this.state.have_garmin_health_token)
+								&&<div>
+									  <h3 id="link_style">Set up Links to Garmin</h3>
+									  <a href='/users/connect_request_token'>Garmin Connect</a><br/>
+							  	  </div>
 							}
-							{this.state.have_fitbit_tokens == false &&
+
+							{!this.state.have_fitbit_token &&
 							  <div>
 							  <h3 id="link_style">Set up Link to Fitbit</h3>
 							  <a href='/fitbit/request_token_fitbit'>Connect to Fitbit</a><br/>
-							  {/*{this.fitBitTokenModel()}*/}
 							  </div>
 							}
 							  <h3 id="link_style">Other</h3>
@@ -199,6 +156,7 @@ class Dashboard extends Component {
 						</div>
 					</div>
 				</div>
+				{this.noDeviceLinkedModel()}
 			</div>
 		);
 	}
