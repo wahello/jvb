a77833f849980c8b2ce26bb224a3b4f742af9717
kns7works <naveen@s7works.io>
2018-12-21 19:21:46 +0530

    returning weather data to ui in dict format

--- user_input/utils/daily_activity.py
+++ user_input/utils/daily_activity.py
@@ -13,8 +13,13 @@ def get_activity_base_format(activity):
     del (activity['activity_data'], activity['user_id'], \
         activity['id'], (activity['created_at']),  \
             activity["activity_weather"], activity['activity_id'])
-    return {**activity_data_dict, **activity, 
-            'activity_weather':activity_weather_dict}
+    weather_keys = ('temperature_feels_like', 'weather_condition', 'humidity', 'dewPoint', 'wind', 'temperature')
+    activity_weathers = {}
+    for k  in weather_keys:
+        if activity_weather_dict and activity_weather_dict[k]:
+            # if activity_weather_dict[k]:
+            activity_weathers[k] = activity_weather_dict[k]['value']
+    return {**activity_data_dict, **activity, **activity_weathers}
 
 def get_daily_activities_in_base_format(user,date,include_all=False):
     '''
