95e48e27441a7faaf66ac511ca2c4355af4561c4
Atul Kumar <atulk@s7inc.co>
2018-12-28 05:57:18 -0500

    Updated message language

--- assets/js/components/registration/UserNotInvited.jsx
+++ assets/js/components/registration/UserNotInvited.jsx
@@ -15,7 +15,7 @@ const userNotInvited = (props) => {
 					</div>
 					<div>
 						Please contact <span style={{fontWeight:"bold"}}>info@jvbwellness.com</span> to
-						request an invitation and then try again after getting approved
+						request an invitation to use this site
 					</div>
 					<CardLink href="/"> Go to Home</CardLink>
 				</div>
