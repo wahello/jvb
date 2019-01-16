8492a40d2ab48e2a96b497adcad379fa0e7d6590
eswars7 <suresh@s7worksio>
2019-01-16 17:10:41 +0530

    In heart_rate_other_data_file, added states of toggle button also.

--- assets/js/components/HRR/heart_rate_other_data_file.jsx
+++ assets/js/components/HRR/heart_rate_other_data_file.jsx
@@ -16,6 +16,7 @@ import {updateHeartData} from '../../network/heart_cal';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 import {renderHrrSelectedDateFetchOverlay} from '../dashboard_healpers';
+
 class Other_Hrr_Data extends Component{
 	constructor(props){
 		 super(props);
@@ -63,8 +64,8 @@ class Other_Hrr_Data extends Component{
 			diff_actity_hrr:"",
 			pure_time_99:"",
 			end_time_activity:"",
-			HRR_activity_start_time:""
-
+			HRR_activity_start_time:"",
+            update_toggle:false
 		}
 		this.renderTime = this.renderTime.bind(this);
 		this.renderSecToMin = this.renderSecToMin.bind(this);
@@ -86,6 +87,7 @@ class Other_Hrr_Data extends Component{
 		this.successHeart = this.successHeart.bind(this);
 		this.errorHeart = this.errorHeart.bind(this);
 		this.renderHrrSelectedDateFetchOverlay = renderHrrSelectedDateFetchOverlay.bind(this);
+		
 	}
 	componentWillReceiveProps(nextProps) {
 		this.setState({ editable: nextProps.hrr.editable });  
@@ -110,6 +112,7 @@ class Other_Hrr_Data extends Component{
 			editable_pure_time_99:!this.state.editable_pure_time_99,
 		})
 	}
+
 	editEndHeartrateActivity(){
 		this.setState({
 			editable_end_heartrate_activity:!this.state.editable_end_heartrate_activity,
@@ -293,8 +296,16 @@ class Other_Hrr_Data extends Component{
 				"end_heartrate_activity":this.state.end_heartrate_activity,
 				"heart_rate_down_up":this.state.heart_rate_down_up,
 				"pure_1min_heart_beats":this.state.pure_1min_heart_beats,
-				"pure_time_99":pure_time_99
+				"pure_time_99":pure_time_99,
+				/*changes made here */
+				"update_toggle":!this.state.update_toggle,
+				/*changes made here */
+
 			},() => {
+				/*changes made here */
+                this.props.updateToggleButton(this.state.update_toggle);
+				/*changes made here */
+
 				let data = {
 	  				"end_time_activity":endTimeActivity.utc().valueOf(),
 					"diff_actity_hrr":diff_time,
@@ -302,10 +313,11 @@ class Other_Hrr_Data extends Component{
 					"end_heartrate_activity":parseInt(this.state.end_heartrate_activity),
 					"heart_rate_down_up":parseInt(this.state.heart_rate_down_up),
 					"pure_1min_heart_beats":parseInt(this.state.pure_1min_heart_beats),
-					"pure_time_99":pure_time_99
+					"pure_time_99":pure_time_99,
+				
 				}
-
-	  			updateHeartData(data, this.props.selectedDate, this.successHeart, this.errorHeart);
+            
+	  			updateHeartData(data, this.props.selectedDate, this.successHeart, this.errorHeart,HeartRateCal.handleChange);
 	  				  				  			
 	  			//this.props.renderHrrData(data);
 			});
@@ -658,7 +670,7 @@ class Other_Hrr_Data extends Component{
 	          	    </div>
           	  </div>
           	  	<div className = "row justify-content-center">
-          	    	<Button onClick = {this.updateData}>Update</Button>
+					  <Button onClick ={this.updateData}>Update</Button>
           	    </div>
 			</div>
 			);
