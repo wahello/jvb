2c836dbee2f6c1027a4529baf1e478121befab47
Atul Kumar <atulk@s7inc.co>
2019-01-30 21:23:03 +0530

    Refactored notify_users_task to take list of recepient emails instead of single email

--- registration/email.py
+++ registration/email.py
@@ -1,17 +1,17 @@
 from django.contrib.auth.models import User
 from django.core.mail import EmailMessage
 
-def user_email_confirmation(user_mail,email_subject,email_body,notify_admins=False):
+def user_email_confirmation(recipient_email,email_subject,email_body,notify_admins=False):
 	
 	admin_users_email = []
 	if(notify_admins):
 		admin_users_email = [u.email for u in User.objects.filter(is_staff=True)]
 
-	if user_mail:
+	if recipient_email:
 		email_object = EmailMessage()
 		email_object.subject = email_subject
 		email_object.body = email_body
 		email_object.from_email = "info@jvbwellness.com"
-		email_object.to = [user_mail]
+		email_object.to = recipient_email
 		email_object.bcc = admin_users_email
 		email_object.send()
