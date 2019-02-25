5af3035680b14e2c3c74ef383d0830f24bb57114
Saipriya <saipriya@s7works.io>
2019-02-25 15:43:36 +0530

    Create ping notifications for Apple

--- apple/views.py
+++ apple/views.py
@@ -4,20 +4,83 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from django.http import Http404
+from django.http import Http404,HttpResponse
 
 from .serializers import UserAppleDataStepsSerializer
 
-from .models import UserAppleDataSteps
+from .models import UserAppleDataSteps,ApplePingNotification
+import datetime
 
+
+def process_notification(user,summary_type,state):
+	""" This function will create instance of ping notification
+	Args:
+		user(instance): user instance
+		summary_type(str): which data
+		state(str): state of current ping notification
+	Return:
+		none
+	"""
+	
+	time=datetime.datetime.now().timestamp()
+	notification="Steps"
+	instance=ApplePingNotification(user=user,
+		upload_start_time_seconds=time,
+		summary_type=summary_type,
+		state=state,
+		notification=notification)
+	instance.save()
+	return instance
+
+def update_notification(instance):
+	"""This function will update state in instance of ping notification
+	Args:
+		instance(state): instance
+	Return:
+		none
+	"""
+
+	instance.state='processing'
+	instance.save()
+
+def update_notification2(instance):
+	"""This function will update state in instance of ping notification
+	Args:
+		instance(state): instance
+	Return:
+		none
+	"""
+
+	instance.state='processed'
+	instance.save()
+
+def error_notification(instance):
+	"""This function will update state in instance of ping notification
+	Args:
+		instance(state): instance
+	Return:
+		none
+	"""
+
+	instance.state='failed'
+	instance.save()
+	
 class UserAppleDataStepsView(generics.CreateAPIView):
+	
 	permission_classes = (IsAuthenticated,)
 	serializer_class = UserAppleDataStepsSerializer
-
 	def post(self,request):
+		
+		user = request.user
+		summary_type = "steps"
+		state = "unprocessed"
+		instance=process_notification(user,summary_type,state)
 		serializer= UserAppleDataStepsSerializer(data= request.data, partial=True)
+		update_notification(instance)
 		if serializer.is_valid():
 			serializer.save()
-			return Response(serializer.data,status=status.HTTP_201_CREATED)
-		return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
- 
+			update_notification2(instance)
+			return Response(serializer.data,status=status.HTTP_201_CREATED)	
+		else:
+			error_notification(instance)
+			return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
