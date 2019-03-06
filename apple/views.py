7f50f20fe2683e83b038fd602d6e2a8f2f6f31bd
Saipriya <saipriya@s7works.io>
2019-03-06 18:28:05 +0530

    First commit

--- apple/views.py
+++ apple/views.py
@@ -1,3 +1,4 @@
+import logging
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.views import APIView
@@ -5,12 +6,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from django.http import Http404,HttpResponse
-
 from .serializers import UserAppleDataStepsSerializer
-
-from .models import UserAppleDataSteps,ApplePingNotification
+from apple.models import UserAppleDataSteps,ApplePingNotification
 import datetime
-
 
 def process_notification(user,summary_type,state):
 	""" This function will create instance of ping notification
@@ -21,7 +19,7 @@ def process_notification(user,summary_type,state):
 	Return:
 		none
 	"""
-	
+
 	time=datetime.datetime.now().timestamp()
 	notification="Steps"
 	instance=ApplePingNotification(user=user,
@@ -64,23 +62,42 @@ def error_notification(instance):
 
 	instance.state='failed'
 	instance.save()
+
+def update_steps_data(user_instance,data):
+	"""This function will update user's steps data given by user
+	Args:
+		user_instance:user instance
+	Reurn:
+		data
+	"""
+	user_instance.data = data
+	user_instance.save()
+	return Response(data,status=status.HTTP_201_CREATED)
+
 	
 class UserAppleDataStepsView(generics.CreateAPIView):
 	
 	permission_classes = (IsAuthenticated,)
 	serializer_class = UserAppleDataStepsSerializer
+
 	def post(self,request):
-		
 		user = request.user
 		summary_type = "steps"
-		state = "unprocessed"
-		instance=process_notification(user,summary_type,state)
-		serializer= UserAppleDataStepsSerializer(data= request.data, partial=True)
-		update_notification(instance)
-		if serializer.is_valid():
-			serializer.save()
-			update_notification2(instance)
-			return Response(serializer.data,status=status.HTTP_201_CREATED)	
-		else:
-			error_notification(instance)
-			return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+		state="unprocessed"
+		obj_date=request.data.get('belong_to')
+		updated_data = request.data.get('data')
+		try:
+			obj=UserAppleDataSteps.objects.get(user=user,belong_to__contains=obj_date)
+			return update_steps_data(obj,updated_data)
+			logging.exception("message")
+		except UserAppleDataSteps.DoesNotExist:
+			instance=process_notification(user,summary_type,state)
+			serializer= UserAppleDataStepsSerializer(data= request.data, partial=True)
+			update_notification(instance)
+			if serializer.is_valid():
+				serializer.save()
+				update_notification2(instance)
+				return Response(serializer.data,status=status.HTTP_201_CREATED)	
+			else:
+				error_notification(instance)
+				return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
