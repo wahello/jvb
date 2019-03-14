8fb43be0a2dee8067c0998153f41030f70eb15f9
dks7inc <dileepk@s7inc.co>
2019-03-14 14:48:50 +0530

    Added activities url

--- apple/urls.py
+++ apple/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from apple import views
 
 urlpatterns = [
-    url(r'^users/datasteps$',views.UserAppleDataStepsView.as_view(), name="apple_data_steps"),
+    url(r'^users/datasteps$',views.UserAppleDataStepsView.as_view(), 
+    	name="apple_data_steps"),
+    url(r'^users/dataactivities$',views.UserAppleDataActivitiesView.as_view(), 
+    	name="apple_data_activities"),
 ]
