127725a2c72ee9b6567e7d6afd9c3e64dca56039
dks7inc <dileepk@s7inc.co>
2019-04-01 16:55:03 +0530

    Added serialzer for aa custom range

--- common/urls.py
+++ common/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 	url(r'users/last_synced', views.UserLastSyncedItemview.as_view(), name="User Last sync time"),
 	url(r'users/have_tokens', views.HaveTokens.as_view(), name="have_tokens"),
 	url(r'users/userrequestbackfill', views.UserBackfillRequestView.as_view(),name="user_backfill_request"),
+	url(r'users/aa_custom_ranges', views.AACustomRangesView.as_view(),name="user_aa_custom_ranges"),
 ]
