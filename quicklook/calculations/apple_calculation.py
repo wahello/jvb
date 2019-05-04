fe443a405ef220d952d89f7382bcc915116bb0ae
dks7inc <dileepk@s7inc.co>
2019-05-05 00:15:33 +0530

    added exception in apple quicklook

--- quicklook/calculations/apple_calculation.py
+++ quicklook/calculations/apple_calculation.py
@@ -3,6 +3,7 @@ import ast
 import pytz
 import json
 from user_input.models import UserDailyInput
+import logging
 
 from quicklook.serializers import UserQuickLookSerializer
 from django.db.models import Q
@@ -455,17 +456,22 @@ def create_apple_quick_look(user,from_date=None,to_date=None):
 			UserAppleDataActivities,user,current_date.date(),current_date.date())
 		
 		if todays_activity_data:
-			todays_activity_data = [ast.literal_eval(todays_activity_data[0])]
-			todays_activity_data = list(map(apple_to_garmin_activities,
-				todays_activity_data))
+			try:
+				todays_activity_data = [ast.literal_eval(todays_activity_data[0])]
+				todays_activity_data = list(map(apple_to_garmin_activities,
+					todays_activity_data))
 
-			combined_user_exercise_activities,combined_user_exec_non_exec_activities =\
-				quicklook.calculations.garmin_calculation.\
-					get_filtered_activity_stats(
-						todays_activity_data[0],user_age,
-						userinput_activities = userinput_activities,
-						epoch_summaries = todays_epoch_data,
-						provide_all=True)
+				combined_user_exercise_activities,combined_user_exec_non_exec_activities =\
+					quicklook.calculations.garmin_calculation.\
+						get_filtered_activity_stats(
+							todays_activity_data[0],user_age,
+							userinput_activities = userinput_activities,
+							epoch_summaries = todays_epoch_data,
+							provide_all=True)
+			except:
+				logging.exception("message")
+				combined_user_exercise_activities = []
+				combined_user_exec_non_exec_activities = []
 		else:
 			combined_user_exercise_activities = []
 			combined_user_exec_non_exec_activities = []
